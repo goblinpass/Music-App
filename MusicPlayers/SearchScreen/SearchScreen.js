73622b52bc128f5ce2ecc3e9e800eb9bd73b40be
WeiDai15 <130736694+WeiDai15@users.noreply.github.com>
2023-12-10 16:02:32 -0700

    Add files via upload

--- MusicPlayers/SearchScreen/SearchScreen.js
+++ MusicPlayers/SearchScreen/SearchScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, Button, FlatList, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, Button, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
 import axios from 'axios';
 
 const SearchScreen = ({ navigation }) => {
@@ -8,86 +8,97 @@ const SearchScreen = ({ navigation }) => {
   const [token, setToken] = useState('');
 
   useEffect(() => {
-    // Use your Client ID and Client Secret obtained from your Spotify App
+    // This should be done in a secure environment
     const CLIENT_ID = 'cdef2c9f3adc4ec18fec1eb184f41ae9';
     const CLIENT_SECRET = '3c07f57baccb498296010da9866aa0dc';
 
-    // Fetch the access token using the Client Credentials Flow
     const fetchToken = async () => {
       try {
         const response = await axios.post(
           'https://accounts.spotify.com/api/token',
-          `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+          `grant_type=client_credentials`,
           {
             headers: {
               'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': `Basic ${Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString('base64')}`,
             },
           }
         );
         setToken(response.data.access_token);
       } catch (error) {
-        console.error('Error obtaining access token:', error.message);
+        console.error('Error obtaining access token:', error);
       }
     };
 
     fetchToken();
   }, []);
 
-  useEffect(() => {
-    // Fetch search results from Spotify API
-    const fetchSearchResults = async () => {
-      try {
-        const response = await axios.get(
-          `https://api.spotify.com/v1/search?q=${query}&type=track,artist`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+  const performSearch = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.spotify.com/v1/search?q=${encodeURIComponent(query)}&type=track,artist`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
-        setSearchResults(response.data.tracks.items);
-      } catch (error) {
-        console.error('Error fetching search results:', error.message);
-      }
-    };
-
-    if (query && token) {
-      fetchSearchResults();
-    } else {
-      // Clear results when the query is empty
-      setSearchResults([]);
+      setSearchResults(response.data.tracks.items);
+    } catch (error) {
+      console.error('Error fetching search results:', error);
     }
-  }, [query, token]);
+  };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Search for songs or artists"
         value={query}
         onChangeText={setQuery}
       />
-      <Button title="Search" onPress={() => {}} />
+      <Button title="Search" onPress={performSearch} />
       <FlatList
         data={searchResults}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <TouchableOpacity
-            onPress={() => navigation.navigate('Player', { artist: item, token })}
+            style={styles.item}
+            onPress={() => navigation.navigate('PlayerScreen', { artist: item, token })} 
           >
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-              <Text>{item.name}</Text>
-              <TouchableOpacity
-                onPress={() => navigation.navigate('Player', { artist: item, token })}
-              >
-                <Text>▶️ Play</Text>
-              </TouchableOpacity>
-            </View>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.play}>▶️ Play</Text>
           </TouchableOpacity>
         )}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+  },
+  name: {
+    flex: 1,
+  },
+
+});
 
 export default SearchScreen;
